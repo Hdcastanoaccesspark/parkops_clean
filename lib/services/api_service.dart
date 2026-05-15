0ae162d859badeceb0393dc179264ae9e3a89a33
KeyB99 <key.barreraaccesspark@gmail.com>
2026-05-14 21:06:50 -0500

    Se agrega ip local

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-const String API_BASE_URL = "https://chitchat-careless-mystify.ngrok-free.dev";
+const String API_BASE_URL = "http://192.168.10.109:10000";
 
 class ApiService {
   static Future<Map<String, dynamic>?> login(
